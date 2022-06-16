feba21c53a9e8e6f5fde07e325eeba2faabd9501
Valentin Valverde <valenreta.developer@gmail.com>
2022-06-16 19:51:32 +0200

    adding auto loggin with token

--- src/apps/PodcastApp/backend/routes/auth.routes.ts
+++ src/apps/PodcastApp/backend/routes/auth.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { LoginController } from "../controllers/Auth/Login.controller";
+import { LoginWithTokenController } from "../controllers/Auth/LoginWithToken.controller";
 import { SignupController } from "../controllers/Auth/Signup.controller";
+import { ValidateJWTMiddlware } from "../middlewares/ValidateJWT.middleware";
 
 export const authRouter = Router();
 
 authRouter.post("/signup", new SignupController().run);
 authRouter.post("/login", new LoginController().run);
+authRouter.post(
+  "/login/token",
+  [new ValidateJWTMiddlware().handle],
+  new LoginWithTokenController().run
+);
