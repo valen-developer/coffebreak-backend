01484e81b4146a8ad8f490b2c9c180a83ff0a88b
Valentin Valverde <valenreta.developer@gmail.com>
2022-06-25 22:39:21 +0200

    fix($application): throw NotUserFound exception

--- src/context/PodcastApp/User/application/UserFinder.ts
+++ src/context/PodcastApp/User/application/UserFinder.ts
@@ -1,3 +1,4 @@
+import { NotFoundUserException } from "../domain/exceptions/NotUserFound.exception";
 import { UserRepository } from "../domain/interfaces/UserRepository.interface";
 import { User } from "../domain/User.mode";
 
@@ -5,10 +6,22 @@ export class UserFinder {
   constructor(private userRepository: UserRepository) {}
 
   public async findByUuid(uuid: string): Promise<User> {
-    return this.userRepository.findByUuid(uuid);
+    const user = await this.userRepository.findByUuid(uuid);
+
+    if (!user) {
+      throw new NotFoundUserException();
+    }
+
+    return user;
   }
 
   public async findByEmail(email: string): Promise<User> {
-    return this.userRepository.findByEmail(email);
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new NotFoundUserException();
+    }
+
+    return user;
   }
 }
