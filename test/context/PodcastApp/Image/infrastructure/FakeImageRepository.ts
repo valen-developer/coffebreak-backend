f86b15bb02dc4d23f897d764a341f69a64df5cd0
Valentin Valverde <valenreta.developer@gmail.com>
2022-06-25 23:14:06 +0200

    fix($infrastructure): implement methods

--- test/context/PodcastApp/Image/infrastructure/FakeImageRepository.ts
+++ test/context/PodcastApp/Image/infrastructure/FakeImageRepository.ts
@@ -2,22 +2,63 @@ import { Image } from "../../../../../src/context/PodcastApp/Image/domain/Image.
 import { ImageRepository } from "../../../../../src/context/PodcastApp/Image/domain/interfaces/ImageRepository.interface";
 
 export class FakeImageRepository implements ImageRepository {
+  private images: Image[] = [];
+
   save(image: Image): Promise<Image> {
-    throw new Error("Method not implemented.");
+    // check if image already exists
+    const existingImage = this.images.find(
+      (i) => i.uuid.value === image.uuid.value
+    );
+
+    if (existingImage) {
+      return Promise.resolve(existingImage);
+    }
+
+    this.images.push(image);
+    return Promise.resolve(image);
   }
+
   findByUuid(uuid: string): Promise<Image> {
-    throw new Error("Method not implemented.");
+    const image = this.images.find((i) => i.uuid.value === uuid);
+
+    if (!image) {
+      throw new Error("Image not found");
+    }
+
+    return Promise.resolve(image);
   }
+
   findByEntityUuid(entityUuid: string): Promise<Image[]> {
-    throw new Error("Method not implemented.");
+    const images = this.images.filter((i) => i.entityUuid.value === entityUuid);
+
+    if (!images) {
+      throw new Error("Images not found");
+    }
+
+    return Promise.resolve(images);
   }
+
   deleteByUuid(uuid: string): Promise<void> {
-    throw new Error("Method not implemented.");
+    this.images = this.images.filter((i) => i.uuid.value !== uuid);
+
+    return Promise.resolve();
   }
+
   deleteByEntityUuid(entityUuid: string): Promise<void> {
-    throw new Error("Method not implemented.");
+    this.images = this.images.filter((i) => i.entityUuid.value !== entityUuid);
+
+    return Promise.resolve();
   }
+
   update(image: Image): Promise<Image> {
-    throw new Error("Method not implemented.");
+    const existingImage = this.images.find(
+      (i) => i.uuid.value === image.uuid.value
+    );
+
+    if (!existingImage) {
+      throw new Error("Image not found");
+    }
+
+    return Promise.resolve(image);
   }
 }
