9f523f80cb4924af16c323d5085c3cd1544d9b3c
Valentin Valverde <valenreta.developer@gmail.com>
2022-07-28 01:58:49 +0200

    feat($devops): env different enviroments

--- .decode_env.sh
+++ .decode_env.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 
+
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ENV_PASSPHRASE" \
---output ./.env .env.gpg
+--output ./.env .env.$ENVIROMENT.gpg
--- .encode_env.sh
+++ .encode_env.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
- gpg  --passphrase="$ENV_PASSPHRASE" --batch --yes  --symmetric --cipher-algo AES256 .env
+# array of string called enviroments with "dev" and "production"
+enviroments=("dev" "production" "local" "staging")
+
+for env in "${enviroments[@]}"
+do
+  echo "Encoding $env"
+  gpg  --passphrase="$ENV_PASSPHRASE" --batch --yes  --symmetric --cipher-algo AES256 .env.$env
+done
